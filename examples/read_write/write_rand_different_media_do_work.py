a4004dc0d9753963e17e86afd017adfabb61ca3b
Moshik Hershcovitch <moshik1@gmail.com>
2022-03-30 04:40:01 -0700

    add numpysave to rand_write

--- examples/read_write/write_rand_different_media_do_work.py
+++ examples/read_write/write_rand_different_media_do_work.py
@@ -16,10 +16,9 @@ G = 1024*M
 size = 0
 np.random.seed(2)
 array_size_mb = 0
-pymm_size_mb = 340*1024
+pymm_size_mb = 150*1024
 pymm_shelf_factor = 3
 remove_write = False
-
 
 def clear_caches ():
     os.system("free -m; sync; echo 3 > /proc/sys/vm/drop_caches; swapoff -a && swapon -a;")
@@ -41,7 +40,6 @@ def numpymemmap_func(path, size_GB, array_data, array_pos, persist_at_the_end):
     mmap_array[:] = array[:]
     mmap_array.flush()
     results["open_time"] = time.time() - t0
-    print(np.amax(mmap_array))
     t0_sum = 0; t1_sum = 0
     t0 = time.time()
     for i in range(array_pos.shape[0]):
@@ -68,49 +66,11 @@ def numpymemmap_func(path, size_GB, array_data, array_pos, persist_at_the_end):
     return results
 
 
-
-
-
-##################################################################
-# Deep copy from DRAM to DRAM                                    #
-##################################################################
-def dram_dram_func():
-    results = { "open_time": 0, "copy_time" : 0, "close_persist_time" : 0}
-    t0 = time.time()
-    array1 = np.copy(array)
-    results["copy_time"] = time.time() - t0
-    print ("[DRAM->DRAM] The time to copy a random float array of %u MB is %0.2f sec" % (int(array.nbytes/1024/1024), (results["copy_time"])))
-    return results        
-
-##################################################################
-# Copy from DRAM to a file located on file systemi               #
-##################################################################
-
-def dram_pickle_func(path):
-    if not os.path.exists(path):
-            os.makedirs(path)
-    filename = path + '/pickle_write'
-    results = { "open_time": 0, "copy_time" : 0, "close_persist_time" : 0}
-    print(filename)
-    t0 = time.time()
-    fileObject = open(filename, 'wb')
-    results["open_time"] = time.time() - t0
-    t0 = time.time()
-    pkl.dump(array, fileObject, protocol=4)
-    results["copy_time"] = time.time() - t0
-    print ("[DRAM->pickle]  The time to copy a random float array of %u MB is %0.2f sec" %  (int(array.nbytes/1024/1024), results['copy_time']))
-    t0 = time.time()
-    fileObject.close()
-    results['close_persist_time'] = time.time() - t0
-    if (remove_write):
-        os.remove(filename)
-    return results
-
 ##################################################################
 # Use PyMM library benchmark PM[FS-DAX]:                         #
 # from DRAM to PM[FS-DAX]                                        #
 ##################################################################
-def pymm_fs_dax_func (path, size_GB, array_data, array_pos, persist_at_the_end):
+def pymm_func (path, size_GB, array_data, array_pos, persist_at_the_end):
     if not os.path.exists(path):
             os.makedirs(path)
     print (path)
@@ -122,14 +82,16 @@ def pymm_fs_dax_func (path, size_GB, array_data, array_pos, persist_at_the_end):
     s.array.persist()
     results["open_time"] = time.time() - t0
     t0_sum = 0; t1_sum = 0
-    print(np.amax(s.array))
     t0 = time.time()
+    itemsize = s.array.itemsize
     for i in range(array_pos.shape[0]):
-        s.array[array_pos[i]] = array_data[i]
+        array_ind = array_pos[i]
+        s.array[array_ind] = array_data[i]
         if (not persist_at_the_end):
             t0_sum += time.time() - t0
             t1 = time.time()
-            s.array.persist()
+            s.array.persist_offset(array_ind,1)
+#            exit(0)
             t1_sum += time.time() - t1
             t0 = time.time()
     if (persist_at_the_end):
@@ -139,91 +101,54 @@ def pymm_fs_dax_func (path, size_GB, array_data, array_pos, persist_at_the_end):
         t1_sum = time.time() - t1
     results["copy_time"] = t0_sum
     results['close_persist_time'] = t1_sum
-    print(np.amax(s.array))
-    print ("[DRAM->numpymemmap]  open_time  %0.2f sec" %   results['open_time'])
-    print ("[DRAM->numpymemmap]  copy_time %0.2f sec" %   results['copy_time'])
-    print ("[DRAM->numpymemmap]  persist_time %0.2f sec" % results['close_persist_time'])
+    print ("[DRAM->pymm]  open_time  %0.2f sec" %   results['open_time'])
+    print ("[DRAM->pymm]  copy_time %0.2f sec" %   results['copy_time'])
+    print ("[DRAM->pymm]  persist_time %0.2f sec" % results['close_persist_time'])
     if (remove_write):
         os.remove(path + "/" + path_split[1] + ".data")
         os.remove(path + "/" + path_split[1] + ".map")
     return results
 
 
-##################################################################
-# Use PyMM library benchmark PM[DEV-DAX]:                        #
-# from DRAM to PM[DEV-DAX]                                       #
-##################################################################
-
-def pymm_dev_dax_func(path, array_data, array_pos, persist_at_the_end):
-    results = { "open_time": 0, "copy_time" : 0, "close_persist_time" : 0}
-    t0 = time.time()
-    s1 = pymm.shelf('pymm_dev_dax',size_mb=pymm_size_mb,pmem_path=path,force_new=True)
-    results["open_time"] = time.time() - t0
-
-    t0 = time.time()
-    s1.array = array
-    s1.array[0].persist()
-    results["copy_time"] = time.time() - t0
-    print ("[DRAM->PM[DEV-DAX(PyMM)]  The time to copy a random float array of %u MB is %0.2f sec" %  (int(array.nbytes/1024/1024), results["copy_time"]))
-    t0 = time.time()
-    results["close_persist_time"] = time.time() - t0
-    print ("[PM->PM[DEV-DAX(PyMM)]  persist %0.2f sec" %  (results["close_persist_time"]))
-
-#    s1.array1 = s1.array
-#    print ("[PM->PM[DEV-DAX(PyMM)]  The time to copy a random float array of %u MB is %0.2f sec" %  (int(array.nbytes/1024/1024), time.time() - t0))
-#    t0 = time.time()
-#    s1.persist()
-#    print ("[PM->PM[DEV-DAX(PyMM)]  persist %0.2f sec" %  (time.time() - t0))
-    return results
-
-##################################################################
-# Use PyMM library benchmark PM[FS-DAX]:                         #
-# from DRAM to NVMe                                              #
-##################################################################
-def pymm_nvme_func(path):
-    if not os.path.exists(path):
-            os.makedirs(path)
-    if os.path.exists(path + '/pymm_nvme.data'):
-        os.remove(path + '/pymm_nvme.data')
-    if os.path.exists(path + '/pymm_nvme.map'):
-        os.remove(path + '/pymm_nvme.map')
-
-    results = { "open_time": 0, "copy_time" : 0, "close_persist_time" : 0}
-    t0 = time.time()
-    s2 = pymm.shelf('pymm_nvme',size_mb=pymm_size_mb,pmem_path=path,force_new=True)
-    results["open_time"] = time.time() - t0
-
-    t0 = time.time()
-    s2.array = array
-    results["copy_time"] = time.time() - t0
-    print ("[DRAM->NMVE(PyMM)] The time to copy a random float array of %u MB is %0.2f sec" % (int(array.nbytes/1024/1024), results["copy_time"]))
-    t0 = time.time()
-    s2.array.persist()
-    results["close_persist_time"] = time.time() - t0
-#    t0 = time.time()
-#    s2.array1 = s2.array
-#    print ("NVME->NVME(PyMM)] The time to copy a random float array of %u MB is %0.2f sec" % (int(array.nbytes/1024/1024), time.time() - t0))
-    if (remove_write):
-        os.remove(path + '/pymm_nvme.data')
-        os.remove(path + '/pymm_nvme.map')
-    return results
-
-def ndarry_save_func(path):
+def ndarry_save_func(path, size_GB, array_data, array_pos, persist_at_the_end):
+    print (path)
     filename = path + '/numpy_save'
     results = { "open_time": 0, "copy_time" : 0, "close_persist_time" : 0}
+    print(filename)
     t0 = time.time()
+    array = np.zeros(int(size_GB) * 1024*1024*128)
     np.save(filename, array)
-    results["copy_time"] = time.time() - t0
-    print ("[ndarry->NVMe (numpy_save)] The time to copy a random float array of %u MB is %0.2f sec" % (int(array.nbytes/1024/1024), results["copy_time"]))
+    results["open_time"] = time.time() - t0
+    t0_sum = 0; t1_sum = 0
+    t0 = time.time()
+    for i in range(array_pos.shape[0]):
+        array[array_pos[i]] = array_data[i]
+        if (not persist_at_the_end):
+            t0_sum += time.time() - t0
+            t1 = time.time()
+            np.save(filename, array)
+            t1_sum += time.time() - t1
+            t0 = time.time()
+    if (persist_at_the_end):
+        t0_sum = time.time() - t0
+        t1 = time.time()
+        np.save(filename, array)
+        t1_sum += time.time() - t1
+        t1_sum = time.time() - t1
+    results["copy_time"] = t0_sum
+    results['close_persist_time'] = t1_sum
+    print(np.amax(array))
+    print ("[DRAM->numpymemmap]  open_time  %0.2f sec" %   results['open_time'])
+    print ("[DRAM->numpymemmap]  copy_time %0.2f sec" %   results['copy_time'])
+    print ("[DRAM->numpymemmap]  persist_time %0.2f sec" % results['close_persist_time'])
     if (remove_write):
-        os.remove(filename + ".npy")
+        os.remove(filename)
     return results
+
+
 
 def run_tests_func(args, array_data, array_pos):
     results = {}
-    if (args.dram):
-        results['dram'] = dram_dram_func()
-
     if (args.numpymemmap_nvme):
         results['numpymmap_nvme'] = numpymemmap_func(args.nvme_path, args.size_GB, array_data, array_pos, args.persist_at_the_end)
     
@@ -233,38 +158,29 @@ def run_tests_func(args, array_data, array_pos):
     if (args.numpymemmap_fs_dax1):
         results['numpymmap_fs_dax1'] = numpymemmap_func(args.fs_dax1_path, args.size_GB, array_data, array_pos, args.persist_at_the_end)
 
-    if (args.pickle_nvme):
-        results['pickle_dram_nvme'] = dram_pickle_func(args.nvme_path)
-    
-    if (args.pickle_fs_dax0): 
-        results['pickle_dram_fs_dax0'] = dram_pickle_func(args.fs_dax0_path)
-    
-    if (args.pickle_fs_dax1): 
-        results['pickle_dram_fs_dax1'] = dram_pickle_func(args.fs_dax1_path)
-    
     if (args.pymm_fs_dax0): 
-        results['pymm_dram_fs_dax0'] = pymm_fs_dax_func(args.fs_dax0_path, args.size_GB, array_data, array_pos, args.persist_at_the_end)
+        results['pymm_dram_fs_dax0'] = pymm_func(args.fs_dax0_path, args.size_GB, array_data, array_pos, args.persist_at_the_end)
     
     if (args.pymm_fs_dax1): 
-        results['pymm_dram_fs_dax1'] = pymm_fs_dax_func(args.fs_dax1_path, args.size_GB, array_data, array_pos, args.persist_at_the_end)
+        results['pymm_dram_fs_dax1'] = pymm_func(args.fs_dax1_path, args.size_GB, array_data, array_pos, args.persist_at_the_end)
     
     if (args.pymm_dev_dax0):
-        results['pymm_dram_dev_dax0'] = pymm_fs_dax_func(args.nvme_path, args.size_GB, array_data, array_pos, args.persist_at_the_end)
+        results['pymm_dram_dev_dax0'] = pymm_func(args.dev_dax0_path, args.size_GB, array_data, array_pos, args.persist_at_the_end)
     
     if (args.pymm_dev_dax1):
-        results['pymm_dram_dev_dax1'] = pymm_fs_dax_func(args.dev_dax1_path,args.size_GB, array_data, array_pos, args.persist_at_the_end)
+        results['pymm_dram_dev_dax1'] = pymm_func(args.dev_dax1_path,args.size_GB, array_data, array_pos, args.persist_at_the_end)
     
     if (args.pymm_nvme):
-        results['pymm_dram_nvme'] = pymm_fs_dax_func(args.nvme_path,args.size_GB, array_data, array_pos, args.persist_at_the_end)
+        results['pymm_dram_nvme'] = pymm_func(args.nvme_path,args.size_GB, array_data, array_pos, args.persist_at_the_end)
     
     if (args.numpy_save_nvme):
-        results['ndarray_save_dram_nvme'] = ndarry_save_func(args.nvme_path)
+        results['ndarray_save_dram_nvme'] = ndarry_save_func(args.nvme_path, args.size_GB, array_data, array_pos, args.persist_at_the_end)
     
     if (args.numpy_save_fs_dax0): 
-        results['ndarray_save_dram_fs_dax0'] = ndarry_save_func(args.fs_dax0_path)
+        results['ndarray_save_dram_fs_dax0'] = ndarry_save_func(args.fs_dax0_path, args.size_GB, array_data, array_pos, args.persist_at_the_end)
     
     if (args.numpy_save_fs_dax1): 
-        results['ndarray_save_dram_fs_dax1'] = ndarry_save_func(args.fs_dax1_path)
+        results['ndarray_save_dram_fs_dax1'] = ndarry_save_func(args.fs_dax1_path, args.size_GB, array_data, array_pos, args.persist_at_the_end)
     
     return results
 
@@ -324,13 +240,9 @@ def main():
     parser.add_argument("--remove_write", action="store_true", default=False, help="not remove the data after write")
     parser.add_argument("--test_all", action="store_true", default=False, help="run all the different options")
     parser.add_argument("--numa_local", action="store_true", default=False, help="run all the different options")
-    parser.add_argument("--dram", action="store_true", default=False, help="deep copy the array to the DRAM")    
     parser.add_argument("--numpymemmap_nvme", action="store_true", default=False, help="save the array as numpy_mmap")    
     parser.add_argument("--numpymemmap_fs_dax0", action="store_true", default=False, help="save the array as numpy_mmap")    
     parser.add_argument("--numpymemmap_fs_dax1", action="store_true", default=False, help="save the array as numpy_mmap")    
-    parser.add_argument("--pickle_nvme", action="store_true", default=False, help="save the array as pickle to NVMe")    
-    parser.add_argument("--pickle_fs_dax0", action="store_true", default=False, help="save the array as pickle to socket 0")    
-    parser.add_argument("--pickle_fs_dax1", action="store_true", default=False, help="save the array as pickle to socket 1")    
     parser.add_argument("--pymm_fs_dax0", action="store_true", default=False, help="save the array to PyMM shelf on fs_daxi [socket 0]")    
     parser.add_argument("--pymm_fs_dax1", action="store_true", default=False, help="save the array to PyMM shelf on fs_dax [socket 1]")    
     parser.add_argument("--pymm_dev_dax0", action="store_true", default=False, help="save the array to PyMM shelf on dev_dax [socket 0]")    
@@ -349,17 +261,18 @@ def main():
     args = parser.parse_args()
     
     
-    num_write_items_8B = 1000*1 
+#    num_write_items_8B = 1000*1000*10 
+    num_write_items_8B = 10*1
     num_write_items_64B = 1000000
     num_write_items_4K = 100
     num_write_items_2M = 10
 
-    array_data = create_rand_data (num_write_items_8B)
+    array_write_data = create_rand_data (num_write_items_8B)
     array_pos = create_rand_pos(num_write_items_8B, int(args.size_GB)*1024*1024*1024/8)
 
 #    clear_caches()
     set_args(args)
-    results = run_tests_func(args, array_data, array_pos)
+    results = run_tests_func(args, array_write_data, array_pos)
     with open(args.output_dir + "_results." + str(args.size_GB) + "GB.json" ,'w') as fp:
             json.dump(results, fp)
 
