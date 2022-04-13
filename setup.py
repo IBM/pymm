82de13628bfadd094ef5bf96864975a4095d9864
Moshik Hershcovitch <moshikh@sg-gpu0.sg.almaden.ibm.com>
2022-04-12 23:23:16 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,49 @@
+# Welcome to the PyMM setup.py.
+#
+#   Build Flags: 
+#      Optimize[Default] / Debug
+#      --debug     
+#  
+
+import subprocess
+import os.path
+import argparse
+from os import path
+
+def build_pymm(args):
+    print ("Updating submodule:")
+    print("git submodule update --init --recursive")
+    os.system("git submodule update --init --recursive")
+    if (not path.exists('build')):
+        print("Directory build Created ")
+        os.mkdir('build')
+    else:
+        print("Directory build already exists")
+    
+    print ("move to build directory")
+    os.chdir('build')
+   
+    if (args.debug):
+        flags = 'DEBUG'
+    else:    
+        flags = 'Release'
+
+    print("cmake -DCMAKE_BUILD_TYPE=" + flags + "-DCMAKE_INSTALL_PREFIX:PATH=`pwd`/dist ..")
+    os.system("cmake -DCMAKE_BUILD_TYPE=" + flags + "-DCMAKE_INSTALL_PREFIX:PATH=`pwd`/dist .. ")
+    
+    print("make bootstrap")
+    os.system("make bootstrap")
+    
+    print("make -j install")
+    os.system("make -j install")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", action="store_true", default=False, help="Build in debug mode")
+    args = parser.parse_args()
+    build_pymm(args)
+
+
+if __name__ == "__main__":
+          main()
